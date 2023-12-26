70938eac81fec29eba5ad9934da0d6666967a5e5
JustinLi3 <Triamethyest@gmail.com>
2023-12-26 16:18:13 -0500

    Progress to obtaining Hexadecimals for sqrt of Prime

--- fic.java
+++ fic.java
@@ -13,52 +13,51 @@ public class fic{
         return binaryRepresentation;
     }
     public static void main(String[] args){
-        Scanner fileInput = new Scanner(System.in);  
-        String test = fileInput.next(); // read in file
-        String binary = ""; 
+        // Scanner fileInput = new Scanner(System.in);  
+        // String test = fileInput.next(); // read in file
+        // String binary = ""; 
 
-        for(int x = 0 ; x<test.length(); x++){  
-            binary += charToBinary(test.charAt(x));  // for each character in file, get the binary string
-        }   
+        // for(int x = 0 ; x<test.length(); x++){  
+        //     binary += charToBinary(test.charAt(x));  // for each character in file, get the binary string
+        // }   
 
-        int binLength = binary.length();
-        int padding = (binary.length()/512)*512 + 512; // Find nearest multiple of 512 bits 
-        int bitRows = padding/32;  // get the amount of rows the binary string can be organized  
-        System.out.println(bitRows);
+        // int binLength = binary.length();
+        // int padding = (binary.length()/512)*512 + 512; // Find nearest multiple of 512 bits 
+        // int bitRows = padding/32;  // get the amount of rows the binary string can be organized  
+        // binary += "1";  // appending bit "1" to the end 
 
+        // String binOfbinLength = Integer.toBinaryString(binLength); // Get the binary of original bit length of file
+        // System.out.println(binOfbinLength);  
 
-        System.out.println(binary);   
+        // while(binary.length()< (padding- binOfbinLength.length())){
+        //     binary+="0"; // append 0's until it is the multiple of 512 bits minus the binary string of the original length away 
+        // }   
+        // binary+=binOfbinLength; // append binary string in order to fufill 512 multiple  
 
-        binary += "1";  // appending bit "1" to the end 
+        // System.out.println(binary);  
+        // System.out.println(binLength);
 
-        String binOfbinLength = Integer.toBinaryString(binLength); // Get the binary of original bit length of file
-        System.out.println(binOfbinLength);  
-
-        while(binary.length()< (padding- binOfbinLength.length())){
-            binary+="0"; // append 0's until it is the multiple of 512 bits minus the binary string of the original length away 
-        }   
-        binary+=binOfbinLength; // append binary string in order to fufill 512 multiple  
-
-        System.out.println(binary);  
-        System.out.println(binLength);
-
-        System.out.println(binary.length());   
-        String[] rows = new String[16]; 
-        for(int x = 0 ; x < bitRows ; x++){
-            rows[x] = binary.substring(x*32,x*32+32);  
-            System.out.println(rows[x]);
-        } 
+        // System.out.println(binary.length());   
+        // String[] rows = new String[16];  //2:39 for N=1 blocks, there are only 16, 32 bit words
+        // for(int x = 0 ; x < bitRows ; x++){
+        //     rows[x] = binary.substring(x*32,x*32+32);  //plug binary into block
+        //     System.out.println(rows[x]);
+        // } 
         String[] hashVals = new String[8];  
-        int[] prime = {2,3,5,7,11,13,17,19};  
-        for(int x = 0 ; x< hashVals.length; x++){ 
-            System.out.println((Math.sqrt(prime[x])* Math.pow(10,32))); 
-            System.out.println(prime[x]);
+        int[] prime = {2,3,5,7,11,13,17,19};  //Setting the initial hash value with the first 32 decimal bits 
+        for(int x = 0 ; x< hashVals.length; x++){   
+            double primeSqrd=  Math.sqrt(prime[x])- (int)Math.sqrt(prime[x]) ;  
+            String bruh = (String.format("%.16f", primeSqrd)).substring(2, 18);  
+            Long test = Long.parseLong(bruh);  
+            String primeBin = Long.toBinaryString(test);
+
+            System.out.println("SQRT of prime number " + prime[x] + " = " + primeBin); 
         }
 
 
 
 
-        fileInput.close();
+        // fileInput.close();
 
     } 
     
