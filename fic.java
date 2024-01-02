894cd1ee4fe7a9ddf4e57e69cbafbd812b2530f0
JustinLi3 <Triamethyest@gmail.com>
2024-01-01 22:57:20 -0500

    Added 64 prime numbers, now starting message schedule

--- fic.java
+++ fic.java
@@ -116,7 +116,7 @@ public class fic{
         // ArrayList<Integer> primes1 = sieve_of_eratosthenes(8);  //Here we are using the Sieve of Eratosthenes algo to find first 8 prime numbers
         // MathContext sqr = new MathContext(34); //Initializing percision 32(decimal) + 2
         // for(int x = 0 ; x< hashVals.length; x++){  
-        //     BigDecimal primeNum = new BigDecimal((primes1.get(x))); //declare each prime as a BigDecimal obj
+        //     BigDecimal primeNum = new BigDecimal(primes1.get(x)); //declare each prime as a BigDecimal obj
         //     BigDecimal primeSqrd = sqrt(primeNum, sqr); //initialize primeSqrd to the output of a squared prime number
         //     primeSqrd = primeSqrd.subtract(BigDecimal.valueOf(primeSqrd.intValue()));  //taking only decimal portion of prime squared
         //     hashVals[x] = decToHexa(primeSqrd);   
@@ -125,7 +125,18 @@ public class fic{
 
         // }  
         //onto constants, first 32 bits of the fractional parts of the cube roots of the first sixty-four prime numbers 
-        //In order to find the first sixty-four prime numbers, we are going to continue using the Sieve of Eratosthenes algorithm 
+        String prime64[] = new String[]{ //64 different keys
+            "428a2f98", "71374491", "b5c0fbcf", "e9b5dba5", "3956c25b", "59f111f1", "923f82a4", "ab1c5ed5",
+            "d807aa98", "12835b01", "243185be", "550c7dc3", "72be5d74", "80deb1fe", "9bdc06a7", "c19bf174",
+            "e49b69c1", "efbe4786", "0fc19dc6", "240ca1cc", "2de92c6f", "4a7484aa", "5cb0a9dc", "76f988da",
+            "983e5152", "a831c66d", "b00327c8", "bf597fc7", "c6e00bf3", "d5a79147", "06ca6351", "14292967",
+            "27b70a85", "2e1b2138", "4d2c6dfc", "53380d13", "650a7354", "766a0abb", "81c2c92e", "92722c85",
+            "a2bfe8a1", "a81a664b", "c24b8b70", "c76c51a3", "d192e819", "d6990624", "f40e3585", "106aa070",
+            "19a4c116", "1e376c08", "2748774c", "34b0bcb5", "391c0cb3", "4ed8aa4a", "5b9cca4f", "682e6ff3", 
+            "748f82ee", "78a5636f", "84c87814", "8cc70208", "90befffa", "a4506ceb", "bef9a3f7", "c67178f2"};  
+        //Preparing message schedule
+
+
 
         
 
