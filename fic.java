28a56b674d4e332bd1dd054a3f4c28410631d532
JustinLi3 <Triamethyest@gmail.com>
2024-01-02 21:30:33 -0500

    Initialized sigma functions and steps for each

--- fic.java
+++ fic.java
@@ -78,7 +78,13 @@ public class fic{
             result += number; 
         }
         return result;
-        }
+        } 
+    public static String sigma1(String bin){ //Here we are initializing sigma 1, right rotation 7 bits, then 18 bits, then shift 3
+        return "";
+    } 
+    public static String sigma0(String bin){ //Here we are initializing sigma 0, right rotation 17 bits, then 19 bits, then shift 10
+        return "";
+    }
 
     public static void main(String[] args){
         Scanner fileInput = new Scanner(System.in);  
@@ -139,11 +145,8 @@ public class fic{
         String[] message = Arrays.copyOf(rows, block*64);  
         //Message formula 
         for(int x = rows.length; x<rows.length+1;x++){
-            // message[x] = sigma1(message[x-2]) + message[x-7] + sigma0(message[x-15]) + message[x-16];  //Message schedule algo
-            System.out.println(message[x-2]); 
-            System.out.println(message[x-7]); 
-            System.out.println(message[x-15]); 
-            System.out.println(message[x-16]); 
+            message[x] = sigma1(message[x-2]) + message[x-7] + sigma0(message[x-15]) + message[x-16];  //Message schedule algo
+    
         } 
 
 
