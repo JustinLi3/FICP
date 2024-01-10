d89a9618a8d2723759b585f5d81bc648b6ba27fd
JustinLi3 <Triamethyest@gmail.com>
2024-01-10 16:18:06 -0500

    Due to Overflow by Hexa parse, we must use Big Integer class, because of immutability, we must reassign values when doing Big Integer functions

--- test.java
+++ test.java
@@ -1,11 +1,18 @@
+import java.math.BigInteger;
 public class test { 
-    public static String hexaTobin(String hexa){  
-        int decimal = Integer.parseInt(hexa,16); //Parse as Hexadecimal
-        String bin = Integer.toBinaryString(decimal); // Parse into Binary 
-        while(bin.length()<32){
-            bin = "0" + bin; //while not 32 bits, append 0's to the front
+    public static String hexaTobin(String hexa){   //Due to inability to directly Parse as a Hexadecimal, we are going to change hexa to decimal into binary manually 
+        BigInteger dec = BigInteger.valueOf(0);  // By the case of overflow, we must use Big Integer
+        int exp = 0;  
+        BigInteger num = BigInteger.valueOf(0); 
+        BigInteger base = BigInteger.valueOf(16);
+        for(int x = hexa.length()-1 ; x>=0; x--){
+            num = BigInteger.valueOf(Character.getNumericValue(hexa.charAt(x)));
+            BigInteger prog = num.multiply(base.pow(exp));
+            dec = dec.add(prog);  // Corrected line: Assign the result back to 'dec' due to immutability
+            exp++;
+            hexa = hexa.substring(0,x); 
         }
-        return bin;
+        return dec.toString();
     } 
     public static void main (String[] args){
         String test ="9b05688c"; 
