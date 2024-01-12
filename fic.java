5ac2c24419492b491be3d0c5ac4c7f948b703901
JustinLi3 <Triamethyest@gmail.com>
2024-01-11 22:54:12 -0500

    Implementing a file path reader
    Importing the file package to help and use try and catch to handle file existence

--- fic.java
+++ fic.java
@@ -1,10 +1,21 @@
+import java.nio.file.Files; 
+import java.nio.file.Path; 
+
 import java.util.Scanner;
 public class fic {
     public static void main(String args[]){
-        Scanner scanner = new Scanner(System.in); 
-        String inputFile = scanner.next(); 
-        String hash = sha_256.SHA(inputFile); 
+        Scanner scanner = new Scanner(System.in);  
+        System.out.println("Please enter the file path: ");
+        String filePath = scanner.nextLine();  
+        try{
+            String hash = sha_256.SHA(filePath); 
+            System.out.println("Hash Result: " + hash); 
+        }  
+        finally{
+            scanner.close();
+
+        }
+        String hash = sha_256.SHA(filePath); 
         System.out.println("Hash Result :" +  hash); 
-        scanner.close();
     }
 }
